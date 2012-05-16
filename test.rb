2651f24954d3b3af0d5db0b087d5f7fce1eb8a6c
Chase Livingston <chaselivingston@gmail.com>
2012-05-15 20:26:09 -0400

    mongodb test

--- test.rb
+++ test.rb
@@ -4,3 +4,12 @@ require 'mongo'
 
 @con = Mongo::Connection.new
 @db = @con['inventory']
+@coll = @db['items']
+
+
+3.times.do |i|
+	@coll.insert({'a' => i+1})
+end
+
+
+puts @coll.find.each {|doc| puts doc.inspect}
